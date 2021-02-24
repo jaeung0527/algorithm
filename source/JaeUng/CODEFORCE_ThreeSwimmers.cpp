5a6fb1ed78492436d31eba398eab21dba6b7b6d5
Jaeung <dlwodnd00@gmail.com>
2021-02-24 20:58:27 +0900

    ADD CODEFORCE_ThreeSwimmers

--- source/JaeUng/CODEFORCE_ThreeSwimmers.cpp
+++ source/JaeUng/CODEFORCE_ThreeSwimmers.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 long long solve(long long p, long long a, long long b, long long c)
 {
-    long long ansA = p % a ? a * ((p / a) + 1) - p : a * ((p / a)) - p;
-    long long ansB = p % b ? b * ((p / b) + 1) - p : b * ((p / b)) - p;
-    long long ansC = p % c ? c * ((p / c) + 1) - p : c * ((p / c)) - p;
+    long long ansA = p % a ? a * ((p / a) + 1) - p : 0;
+    long long ansB = p % b ? b * ((p / b) + 1) - p : 0;
+    long long ansC = p % c ? c * ((p / c) + 1) - p : 0;
     return min(ansA, min(ansB, ansC));
 }
 
